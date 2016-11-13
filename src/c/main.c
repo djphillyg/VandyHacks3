9db2618f80999c3bbb1b05cbb1769fdc37461e3a
vanryzlg <l.vanryzin@vanderbilt.edu>
2016-11-12 23:09:25 -0600

    good

--- src/c/main.c
+++ src/c/main.c
@@ -1,17 +1,17 @@
 #include <pebble.h>
 
-Window *windowWakeup;
+Window *windowWakeup, *windowWakeup2, *windowWakeup3, *windowWakeup4, *windowWakeup5;
 
 TextLayer *text_layer, *text_layer2, *text_layer3;
 
 void up_click_handler_Wakeup(ClickRecognizerRef recognizer, void *context)
 {
-   text_layer_set_text(text_layer, "you pressed Yes");
+  window_stack_push(windowWakeup2, true);
 }
- 
+
 void down_click_handler_Wakeup(ClickRecognizerRef recognizer, void *context)
 {
-   text_layer_set_text(text_layer, "you pressed No");
+   text_layer_set_text(text_layer, "You pressed No");
 }
  
 void select_click_handler_Wakeup(ClickRecognizerRef recognizer, void *context)
@@ -24,6 +24,50 @@ void click_config_provider_Wakeup(void *context)
     window_single_click_subscribe(BUTTON_ID_UP, up_click_handler_Wakeup);
     window_single_click_subscribe(BUTTON_ID_DOWN, down_click_handler_Wakeup);
     window_single_click_subscribe(BUTTON_ID_SELECT, select_click_handler_Wakeup);
+}
+
+void up_click_handler_Wakeup2(ClickRecognizerRef recognizer, void *context)
+{
+  window_stack_push(windowWakeup3, true);
+}
+
+void down_click_handler_Wakeup2(ClickRecognizerRef recognizer, void *context)
+{
+   window_stack_push(windowWakeup4, true);
+}
+ 
+void select_click_handler_Wakeup2(ClickRecognizerRef recognizer, void *context)
+{
+   text_layer_set_text(text_layer, "you pressed select");
+}
+
+void click_config_provider_Wakeup2(void *context)
+{
+    window_single_click_subscribe(BUTTON_ID_UP, up_click_handler_Wakeup2);
+    window_single_click_subscribe(BUTTON_ID_DOWN, down_click_handler_Wakeup2);
+    window_single_click_subscribe(BUTTON_ID_SELECT, select_click_handler_Wakeup2);
+}
+
+void up_click_handler_Wakeup3(ClickRecognizerRef recognizer, void *context)
+{
+  window_stack_push(windowWakeup4, true);
+}
+
+void down_click_handler_Wakeup3(ClickRecognizerRef recognizer, void *context)
+{
+   window_stack_push(windowWakeup5, true);
+}
+ 
+void select_click_handler_Wakeup3(ClickRecognizerRef recognizer, void *context)
+{
+   text_layer_set_text(text_layer, "you pressed select");
+}
+
+void click_config_provider_Wakeup3(void *context)
+{
+    window_single_click_subscribe(BUTTON_ID_UP, up_click_handler_Wakeup3);
+    window_single_click_subscribe(BUTTON_ID_DOWN, down_click_handler_Wakeup3);
+    window_single_click_subscribe(BUTTON_ID_SELECT, select_click_handler_Wakeup3);
 }
 
 void window_load_Wakeup(Window *window)
@@ -48,6 +92,112 @@ void window_load_Wakeup(Window *window)
 void window_unload_Wakeup(Window *window) 
 {
   text_layer_destroy(text_layer);
+  text_layer_destroy(text_layer2);
+  text_layer_destroy(text_layer3);
+}
+
+void window_load_Wakeup2(Window *window)
+{
+  text_layer = text_layer_create(GRect(0,42,96,84));
+  text_layer_set_background_color(text_layer, GColorClear);
+  text_layer_set_text_color(text_layer, GColorBlueMoon);
+  text_layer2 = text_layer_create(GRect(96,0,48,42));
+  text_layer_set_background_color(text_layer2, GColorClear);
+  text_layer_set_text_color(text_layer2, GColorBlueMoon);
+  text_layer3 = text_layer_create(GRect(96,126,48,42));
+  text_layer_set_background_color(text_layer3, GColorClear);
+  text_layer_set_text_color(text_layer3, GColorBlueMoon);
+  layer_add_child(window_get_root_layer(window), text_layer_get_layer(text_layer));
+  layer_add_child(window_get_root_layer(window), text_layer_get_layer(text_layer2));
+  layer_add_child(window_get_root_layer(window), text_layer_get_layer(text_layer3));
+  text_layer_set_text(text_layer, "Do you have 10 fingers?");
+  text_layer_set_text(text_layer2, "Yes!");
+  text_layer_set_text(text_layer3, "No.");
+}
+
+void window_unload_Wakeup2(Window *window) 
+{
+  text_layer_destroy(text_layer);
+  text_layer_destroy(text_layer2);
+  text_layer_destroy(text_layer3);
+}
+
+void window_load_Wakeup3(Window *window)
+{
+  text_layer = text_layer_create(GRect(0,42,96,84));
+  text_layer_set_background_color(text_layer, GColorClear);
+  text_layer_set_text_color(text_layer, GColorBlueMoon);
+  text_layer2 = text_layer_create(GRect(96,0,48,42));
+  text_layer_set_background_color(text_layer2, GColorClear);
+  text_layer_set_text_color(text_layer2, GColorBlueMoon);
+  text_layer3 = text_layer_create(GRect(96,126,48,42));
+  text_layer_set_background_color(text_layer3, GColorClear);
+  text_layer_set_text_color(text_layer3, GColorBlueMoon);
+  layer_add_child(window_get_root_layer(window), text_layer_get_layer(text_layer));
+  layer_add_child(window_get_root_layer(window), text_layer_get_layer(text_layer2));
+  layer_add_child(window_get_root_layer(window), text_layer_get_layer(text_layer3));
+  text_layer_set_text(text_layer, "Can you press your finger through your palm?");
+  text_layer_set_text(text_layer2, "Yes!");
+  text_layer_set_text(text_layer3, "No.");
+}
+
+void window_unload_Wakeup3(Window *window) 
+{
+  text_layer_destroy(text_layer);
+  text_layer_destroy(text_layer2);
+  text_layer_destroy(text_layer3);
+}
+
+void window_load_Wakeup4(Window *window)
+{
+  text_layer = text_layer_create(GRect(0,42,96,84));
+  text_layer_set_background_color(text_layer, GColorClear);
+  text_layer_set_text_color(text_layer, GColorBlueMoon);
+  text_layer2 = text_layer_create(GRect(96,0,48,42));
+  text_layer_set_background_color(text_layer2, GColorClear);
+  text_layer_set_text_color(text_layer2, GColorBlueMoon);
+  text_layer3 = text_layer_create(GRect(96,126,48,42));
+  text_layer_set_background_color(text_layer3, GColorClear);
+  text_layer_set_text_color(text_layer3, GColorBlueMoon);
+  layer_add_child(window_get_root_layer(window), text_layer_get_layer(text_layer));
+  layer_add_child(window_get_root_layer(window), text_layer_get_layer(text_layer2));
+  layer_add_child(window_get_root_layer(window), text_layer_get_layer(text_layer3));
+  text_layer_set_text(text_layer, "You are dreaming :)");
+  text_layer_set_text(text_layer2, "");
+  text_layer_set_text(text_layer3, "");
+}
+
+void window_unload_Wakeup4(Window *window) 
+{
+  text_layer_destroy(text_layer);
+  text_layer_destroy(text_layer2);
+  text_layer_destroy(text_layer3);
+}
+
+void window_load_Wakeup5(Window *window)
+{
+  text_layer = text_layer_create(GRect(0,42,96,84));
+  text_layer_set_background_color(text_layer, GColorClear);
+  text_layer_set_text_color(text_layer, GColorBlueMoon);
+  text_layer2 = text_layer_create(GRect(96,0,48,42));
+  text_layer_set_background_color(text_layer2, GColorClear);
+  text_layer_set_text_color(text_layer2, GColorBlueMoon);
+  text_layer3 = text_layer_create(GRect(96,126,48,42));
+  text_layer_set_background_color(text_layer3, GColorClear);
+  text_layer_set_text_color(text_layer3, GColorBlueMoon);
+  layer_add_child(window_get_root_layer(window), text_layer_get_layer(text_layer));
+  layer_add_child(window_get_root_layer(window), text_layer_get_layer(text_layer2));
+  layer_add_child(window_get_root_layer(window), text_layer_get_layer(text_layer3));
+  text_layer_set_text(text_layer, "You are  awake :)");
+  text_layer_set_text(text_layer2, "");
+  text_layer_set_text(text_layer3, "");
+}
+
+void window_unload_Wakeup5(Window *window) 
+{
+  text_layer_destroy(text_layer);
+  text_layer_destroy(text_layer2);
+  text_layer_destroy(text_layer3);
 }
 
 void init() 
@@ -78,7 +228,29 @@ void init()
       .load = window_load_Wakeup,
       .unload = window_unload_Wakeup,
     });
+    windowWakeup2 = window_create();
+    window_set_window_handlers(windowWakeup2, (WindowHandlers) {
+      .load = window_load_Wakeup2,
+      .unload = window_unload_Wakeup2,
+    });
+    windowWakeup3 = window_create();
+    window_set_window_handlers(windowWakeup3, (WindowHandlers) {
+      .load = window_load_Wakeup3,
+      .unload = window_unload_Wakeup3,
+    });
+    windowWakeup4 = window_create();
+    window_set_window_handlers(windowWakeup4, (WindowHandlers) {
+      .load = window_load_Wakeup4,
+      .unload = window_unload_Wakeup4,
+    });
+    windowWakeup5 = window_create();
+    window_set_window_handlers(windowWakeup5, (WindowHandlers) {
+      .load = window_load_Wakeup5,
+      .unload = window_unload_Wakeup5,
+    });
     window_set_click_config_provider(windowWakeup, click_config_provider_Wakeup);
+    window_set_click_config_provider(windowWakeup2, click_config_provider_Wakeup2);
+    window_set_click_config_provider(windowWakeup3, click_config_provider_Wakeup3);
     window_stack_push(windowWakeup, true);
  // } else {
     
@@ -89,6 +261,8 @@ void deinit()
 {
   //deinitialize app elements
   window_destroy(windowWakeup);
+  window_destroy(windowWakeup2);
+  window_destroy(windowWakeup3);
 }
 
 int main(void) 
