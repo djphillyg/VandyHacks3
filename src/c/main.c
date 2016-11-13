86dc8b100f92c4215e84cd4d0096b1012890fa3c
vanryzlg <l.vanryzin@vanderbilt.edu>
2016-11-12 23:55:36 -0600

    nice

--- src/c/main.c
+++ src/c/main.c
@@ -1,6 +1,6 @@
 #include <pebble.h>
 
-Window *windowWakeup, *windowWakeup2, *windowWakeup3, *windowWakeup4, *windowWakeup5;
+Window *windowWakeup, *windowWakeup2, *windowWakeup3, *windowWakeup4, *windowWakeup5, *window;
 
 TextLayer *text_layer, *text_layer2, *text_layer3;
 
@@ -215,9 +215,21 @@ void window_unload_Wakeup5(Window *window)
   text_layer_destroy(text_layer3);
 }
 
+void window_load(Window *window) 
+{
+  text_layer = text_layer_create(GRect(0,0,144,168));
+  text_layer_set_background_color(text_layer, GColorClear);
+  text_layer_set_text_color(text_layer, GColorBlack);
+  text_layer_set_text(text_layer, "First Wakeup set");
+}
+
+void window_unload(Window *window) {
+  text_layer_destroy(text_layer);
+}
+
 void init() 
 {
-  //if (launch_reason() == APP_LAUNCH_WAKEUP) {
+  if (launch_reason() == APP_LAUNCH_WAKEUP) {
     //app was started by wakeup
     // Let the timestamp be 2 hours from now
     time_t future_timestamp = time(NULL) + (2 * 60 * SECONDS_PER_MINUTE);
@@ -267,9 +279,34 @@ void init()
     window_set_click_config_provider(windowWakeup2, click_config_provider_Wakeup2);
     window_set_click_config_provider(windowWakeup3, click_config_provider_Wakeup3);
     window_stack_push(windowWakeup, true);
- // } else {
-    
-  //}
+  } else {
+
+    time_t future_timestamp = time(NULL) + (1 * SECONDS_PER_MINUTE);
+
+    // Choose a 'cookie' value representing the reason for the wakeup
+    const int cookie = 0;
+
+    // If true, the user will be notified if they missed the wakeup 
+    // (i.e. their watch was off)
+    const bool notify_if_missed = true;
+
+    // Schedule wakeup event
+    WakeupId id = wakeup_schedule(future_timestamp, cookie, notify_if_missed);
+
+    // Check the scheduling was successful
+    if(id >= 0) {
+      // Persist the ID so that a future launch can query it
+      const int wakeup_id_key = 43;
+      persist_write_int(wakeup_id_key, id);
+      
+    window = window_create();
+    window_set_window_handlers(window, (WindowHandlers) {
+      .load = window_load,
+      .unload = window_unload,
+    });
+    window_stack_push(window, true);
+    }
+  }
 }
 
 void deinit() 
